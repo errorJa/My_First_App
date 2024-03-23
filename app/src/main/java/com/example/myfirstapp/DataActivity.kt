8c8968fd24d15be521d98fc3c5ac38b8fbe2aa3e
Egor Nefedov <mr.errorja@gmail.com>
2024-03-23 11:27:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/DataActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DataActivity : AppCompatActivity() {
+    private lateinit var viewText: TextView
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_data)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        viewText = findViewById(R.id.textView)
+        button = findViewById(R.id.button2)
+        viewText.text = intent.getStringExtra("Text")
+
+        button.setOnClickListener {
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myfirstapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var editText: EditText
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+        editText = findViewById(R.id.editTextText)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val text = editText.text.toString()
+            val intent = Intent(this@MainActivity, DataActivity::class.java)
+            intent.putExtra("Text", text)
+            startActivity(intent)
+        }
+    }
+}
